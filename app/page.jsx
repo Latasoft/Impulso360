4d6bdc7a0bdd43ba080de0de8eeae925f6a21b1b
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-08-04 15:45:03 -0400

    llalalal

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
+import impulso360Logo from 'public/images/logob.png'
+import anglesDown from 'public/images/angles-down.svg'
+
 import porQueNosotros from 'public/images/porQueNosotros.webp';
 import portadaFondoB from 'public/images/portadaFondoB.jpg';
 import portadaFondoC from 'public/images/portadaFondoC.jpg';
@@ -65,7 +68,7 @@ useEffect(() => {
 
     return (
         <main className={` relative text-white font-Roboto `}>
-            <section className={` h-screen relative w-full bg-cover bg-center bg-black overflow-hidden  `} >
+            <section className={` h-screen relative w-full bg-cover bg-center bg-black overflow-hidden  `} id={`inicio`}>
                 <div className={` absolute top-0 left-0 w-full h-full `}>
                     <video poster='images/portadaFondoVideo.png' className='object-cover object-center h-full w-full' preload="auto" autoPlay="autoplay" muted loop playsInline>
                         <source src={portadaVideo} type="video/mp4" />
@@ -73,21 +76,22 @@ useEffect(() => {
                 </div>
                 <div className={`  absolute top-0 left-0 w-full h-full bg-fucsiaAzulRatioFondo opacity-40 `} />
                 <div className={` relative mx-auto flex flex-col w-full h-full items-center justify-center px-4`}>
-                    <h2 className={` max-w-5xl text-left w-full transition-all ease-in-out duration-100 text-4xl  lg:text-5xl font-bold lg:my-2 opacity-80`}>En Impulso 360</h2>
-                    <h3 className={` max-w-5xl text-left w-full transition-all ease-in-out duration-100 text-2xl lg:text-3xl font-semibold lg:my-2 opacity-80 `}>comienza tu transformación digital hoy</h3>
-                    <p className={` max-w-5xl text-left w-full text-lg my-8 `}><Link className={` transition-all ease-in-out duration-200 rounded-full py-3 px-6 hover:bg-white bg-[#b10c72] hover:text-black text-white no-underline font-medium`} href='' >Conoce más aquí</Link></p>
+                    <h2 data-aos-duration="3618" data-aos-once="true" data-aos='fade-right' className={` max-w-5xl text-left w-full transition-all ease-in-out text-4xl  lg:text-7xl font-bold lg:my-0 opacity-80`}>En<Image className={` inline-block h-12 lg:h-24 w-auto `} src={impulso360Logo} alt='' /></h2>
+                    <h3 data-aos-duration="3618" data-aos-once="true" data-aos='fade-right' className={` font-RobotoCondensed max-w-5xl text-left w-full transition-all ease-in-out text-3xl lg:text-4xl font-light lg:my-0 opacity-80 `}>comienza tu transformación digital hoy</h3>
+                    <p data-aos-duration="3618" data-aos-once="true"  data-aos='fade-right' className={` max-w-5xl text-left w-full text-lg my-8 `}><Link className={` inline-block transition-all ease-in-out duration-200 text-white no-underline font-medium opacity-80 text-lg lg:text-xl hover:translate-y-1 `} href='#serviciosIntegrales' ><Image className={` inline-block mr-3 h-5 w-auto `} src={anglesDown} alt='Angles Down' /> Conoce más aquí <Image className={` inline-block ml-3 h-5 w-auto`} src={anglesDown} alt='Angles Down' /></Link></p>
+                {/* <p className={` max-w-5xl text-left w-full text-lg my-8 `}><Link className={` transition-all ease-in-out duration-200 rounded-full py-3 px-6 hover:bg-white bg-[#b10c72] hover:text-black text-white no-underline font-medium`} href='' >Conoce más aquí</Link></p> */}
                 </div>
             </section>
-            <section className={` pt-16 bg-black `}>
-                <h3 className={` max-w-5xl mx-auto transition-all ease-in-out duration-100 text-3xl lg:text-4xl font-semibold mb-8 text-center uppercase font-RobotoCondensed opacity-80`}>Servicios integrales</h3>
+            <section className={` pt-16 bg-black `} id={`serviciosIntegrales`}>
+                <h3 className={` max-w-5xl mx-auto transition-all ease-in-out text-3xl lg:text-4xl font-semibold mb-8 text-center uppercase font-RobotoCondensed opacity-80`}>Servicios integrales</h3>
                 {!!serviciosBlocks?.length && (
                     <div className={` mx-auto grid grid-cols-1 md:grid-cols-2 `}>
                         {serviciosBlocks.map((item, index) => (
                             <div key={index} className={` w-full bg-cover bg-center `} style={{ backgroundImage: `url('${item.fondoImagen.src}')`, }}>
-                                <div className={` flex flex-col items-center ${ index % 2 === 0 ? 'md:items-end' : 'md:items-start'} py-28 px-2 md:px-3 lg:px-6 xl:px-12 `}>
-                                    <div className={` py-6 bg-black bg-opacity-70 text-center max-w-md`}>
-                                        <h3 className={`transition-all ease-in-out duration-100 text-3xl lg:text-4xl font-semibold mx-4 my-8 font-RobotoCondensed opacity-80`}>{item.titulo}</h3>
-                                        <p className={`transition-all ease-in-out duration-100 text-lg lg:text-xl m-4 `}>{item.descripcion}</p>
+                                <div className={` transition-all ease-in-out duration-700 flex flex-col items-center ${ index % 2 === 0 ? 'md:items-end' : 'md:items-start'} py-28 px-2 md:px-3 lg:px-6 xl:px-12 `}>
+                                    <div data-aos-duration="3618" data-aos-once="true"  data-aos={index % 2 === 0 ? 'fade-up' : 'fade-up'} className={` transition-all ease-in-out py-8 bg-black bg-opacity-70 text-center max-w-md `} >
+                                        <h3 className={`transition-all ease-in-out text-3xl lg:text-4xl font-semibold mx-4 mb-8 font-RobotoCondensed opacity-80`}>{item.titulo}</h3>
+                                        <p className={`transition-all ease-in-out text-lg lg:text-xl mx-4 `}>{item.descripcion}</p>
                                         {/* <p className={`  text-lg mx-4 my-8 `}><Link className={` transition-all ease-in-out duration-200 rounded-full py-3 px-6 bg-white hover:bg-[#b10c72] text-black hover:text-white no-underline font-medium`} href='' >Cotiza aquí</Link></p> */}
                                     </div>
                                 </div>
@@ -96,43 +100,43 @@ useEffect(() => {
                     </div>
                 )}
             </section>
-            <section className={` relative w-full bg-cover bg-center bg-black overflow-hidden `} >
+            <section className={` relative w-full bg-cover bg-center bg-black overflow-hidden `} id={`porQueNosotros`}>
                 <div className={` absolute top-0 left-0 w-full h-full `}>
                     <video poster='images/portadaFondoVideo.png' className='object-cover object-center h-full w-full' preload="auto" autoPlay="autoplay" muted loop playsInline>
                         <source src={portadaVideo} type="video/mp4" />
                     </video>
                 </div>
-                <div className={` relative max-w-5xl mx-auto grid md:grid-cols-2 grid-cols-1 px-4 pt-16 `}>
+                <div className={` relative max-w-5xl mx-auto grid md:grid-cols-2 grid-cols-1 px-4 md:px-0 pt-16 `}>
                     <div className={` p-4 bg-white bg-opacity-30 `}>
-                        <h3 className={` text-[#b10c72] transition-all ease-in-out duration-100 text-3xl lg:text-4xl font-semibold font-RobotoCondensed mx-4 mb-8 mt-4 `}>¿Por qué nosotros?</h3>
-                        <ul className={` list-disc transition-all ease-in-out duration-100 text-lg ml-8 mr-4 `}>
-                            <li className={` my-4 `}>Nuestro servicio incluye la implementación de un CRM de última generación que optimiza la gestión de leads y seguimientos. Con esta herramienta, podrás mejorar la eficiencia de tu empresa y aumentar tus oportunidades de venta.</li>
-                            <li className={` my-4 `}>Colaboramos estrechamente contigo para comprender tus metas, identificar desafíos específicos y crear estrategias de marketing digital personalizadas.</li>
-                            <li className={` my-4 `}>No brindamos soluciones estándar, sino que diseñamos estrategias que se ajustan de forma precisa a tu visión y objetivos comerciales.</li>
-                            <li className={` my-4 `}>Nos dedicamos a potenciar el crecimiento de tu empresa a través de servicios integrales de marketing digital y la implementación estratégica de CRM.</li>
+                        <h3 className={` text-[#b10c72] transition-all ease-in-out text-3xl lg:text-4xl font-semibold font-RobotoCondensed mx-4 mb-8 mt-4 `}>¿Por qué nosotros?</h3>
+                        <ul className={` list-disc transition-all ease-in-out text-lg ml-8 mr-4 `}>
+                            <li data-aos-duration="3618" data-aos-once="true"  data-aos='fade-right' className={` my-4 `}>Nuestro servicio incluye la implementación de un CRM de última generación que optimiza la gestión de leads y seguimientos. Con esta herramienta, podrás mejorar la eficiencia de tu empresa y aumentar tus oportunidades de venta.</li>
+                            <li data-aos-duration="3618" data-aos-once="true"  data-aos='fade-right' className={` my-4 `}>Colaboramos estrechamente contigo para comprender tus metas, identificar desafíos específicos y crear estrategias de marketing digital personalizadas.</li>
+                            <li data-aos-duration="3618" data-aos-once="true"  data-aos='fade-right' className={` my-4 `}>No brindamos soluciones estándar, sino que diseñamos estrategias que se ajustan de forma precisa a tu visión y objetivos comerciales.</li>
+                            <li data-aos-duration="3618" data-aos-once="true"  data-aos='fade-right' className={` my-4 `}>Nos dedicamos a potenciar el crecimiento de tu empresa a través de servicios integrales de marketing digital y la implementación estratégica de CRM.</li>
                         </ul>
                     </div>
                     <div className={`flex items-center justify-center p-4 text-center bg-white bg-opacity-30 `}>
-                        <Image className={` mx-auto `} src={porQueNosotros} alt='' />
+                        <Image data-aos-duration="3618" data-aos-once="true"  data-aos='flip-left' className={` mx-auto `} src={porQueNosotros} alt='' />
                     </div>
                 </div>
                 <div className={` relative px-4 pt-16`}>
-                    <p className={` opacity-80 max-w-5xl mx-auto px-8 text-center text-white font-light text-2xl lg:text-3xl leading-relaxed lg:leading-relaxed font-RobotoCondensed`}>Nos consideramos socios estratégicos de tu éxito. Nos dedicamos a impulsar tu presencia en línea, aumentar la visibilidad de tu marca y, en última instancia, a potenciar el crecimiento sostenible de tu negocio en el mundo digital.</p>
+                    <p data-aos-duration="3618" data-aos-once="true"  data-aos='fade' className={` opacity-80 max-w-5xl mx-auto px-8 text-center text-white font-light text-2xl lg:text-3xl leading-relaxed lg:leading-relaxed font-RobotoCondensed`}>Nos consideramos socios estratégicos de tu éxito. Nos dedicamos a impulsar tu presencia en línea, aumentar la visibilidad de tu marca y, en última instancia, a potenciar el crecimiento sostenible de tu negocio en el mundo digital.</p>
                 </div>
                 <div className={` relative px-4 py-16`}>
                     <h2 className={` max-w-5xl mx-auto text-center w-full transition-all ease-in-out duration-100 text-3xl lg:text-3xl font-bold lg:my-2 opacity-80 uppercase`}>Nuestros tres pilares esenciales</h2>
 
                     <div className={` mx-auto max-w-5xl grid md:grid-cols-3 grid-cols-1 mt-8 `}>
 
-                        <div className={` mx-auto text-center w-full text-lg bg-white bg-opacity-30 p-8 `}>
+                        <div data-aos-duration="3618" data-aos-once="true"  data-aos='fade-right' className={` mx-auto text-center w-full text-lg bg-white bg-opacity-30 p-8 `}>
                             <h3 className={` text-2xl font-medium mb-4 opacity-80 `}>Enfoque Estratégico Personalizado</h3>
                             <p className={` text-lg`}>Entendemos que cada negocio es único. Trabajamos contigo para comprender tus objetivos, identificar desafíos específicos y desarrollar estrategias de marketing digital a medida. No ofrecemos soluciones genéricas; creamos estrategias que se alinean con tu visión y metas empresariales.</p>
                         </div>
-                        <div className={` mx-auto text-center w-full text-lg bg-[#b10c72] bg-opacity-30 p-8 `}>
+                        <div data-aos-duration="3618" data-aos-once="true"  data-aos='fade-right' className={` mx-auto text-center w-full text-lg bg-[#b10c72] bg-opacity-30 p-8 `}>
                             <h3 className={` text-2xl font-medium mb-4 opacity-80`}>Innovación y Adaptabilidad Continuas</h3>
                             <p className={` text-lg`}>El marketing digital evoluciona constantemente. Nos comprometemos a mantenerte al tanto de las últimas tendencias y tecnologías. Nuestro equipo altamente capacitado se esfuerza por la innovación constante para asegurar que tu presencia en línea esté siempre a la par con las mejores prácticas y te dé una ventaja competitiva sostenible.</p>
                         </div>
-                        <div className={` mx-auto text-center w-full text-lg bg-white bg-opacity-30 p-8 `}>
+                        <div data-aos-duration="3618" data-aos-once="true"  data-aos='fade-right' className={` mx-auto text-center w-full text-lg bg-white bg-opacity-30 p-8 `}>
                             <h3 className={` text-2xl font-medium mb-4 opacity-80`}>Resultados Medibles y Tangibles</h3>
                             <p className={` text-lg`}>No nos conformamos con promesas vacías. Implementamos estrategias con datos y métricas concretas para asegurarnos de que cada campaña genere resultados tangibles.</p>
                         </div>
@@ -143,23 +147,23 @@ useEffect(() => {
                     Algunos de los clientes que han confiado en nosotros
                 </p>
                 <p className={` relative max-w-5xl mx-auto px-8 mt-8 text-center pb-16 `}>
-                                <Image className={` mx-5 inline-block transition-opacity duration-[618ms] ease-in-out ${fade ? 'opacity-100' : 'opacity-0'} `} src={ imagen } alt='' />
+                    <Image className={` mx-5 inline-block transition-opacity duration-[618ms] ease-in-out ${fade ? 'opacity-100' : 'opacity-0'} `} src={ imagen } alt='' />
                 </p>
             </section>
-            <section className={` bg-white `}>
+            <section className={` bg-white `} id={`contacto`}>
                     <p className={` max-w-5xl mx-auto px-8 pt-16 pb-4 text-center text-black font-semibold text-xl lg:text-2xl leading-relaxed lg:leading-relaxed font-RobotoCondensed`}>
                         ¿Aún no estás seguro? Contactanos para ofrecerte una asesoria personalizada y aclarar tus dudas.
                     </p>
                 <div className={` max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 bg-black`}>
                     <div className={` w-full pt-6 md:py-6 text-center text-white font-semibold text-xl lg:text-2xl leading-relaxed lg:leading-relaxed font-RobotoCondensed`}>
 
-                        <form className={` mx-8 appearance-none text-left text-black font-normal text-lg lg:text-xl font-RobotoCondensed `}>
-                            <input className={` block my-4 p-2 w-full appearance-none  `} type='text' placeholder='Nombre' />
-                            <input className={` block my-4 p-2 w-full appearance-none `} type='email' placeholder='Correo' />
-                            <input className={` block my-4 p-2 w-full appearance-none `} type='tel' placeholder='Teléfono' />
-                            <input className={` block my-4 p-2 w-full appearance-none `} type='text' placeholder='Asunto' />
-                            <textarea className={` block my-4 p-2 w-full appearance-none `} rows='8' placeholder='Mensaje' />
-                            <input className={` inline-block mt-4 appearance-none transition-all ease-in-out duration-200 rounded-full py-1 px-12 uppercase hover:bg-white bg-[#b10c72] hover:text-black text-white no-underline font-medium cursor-pointer `} type='Submit' value='Enviar' />
+                        <form className={` mx-8 appearance-none text-left text-black font-normal text-lg lg:text-xl font-RobotoCondensed `} action='https://api.web3forms.com/submit' method='post'>
+                            <input className={` block my-4 p-2 w-full appearance-none text-black bg-neutral-200 `} type='text' placeholder='Nombre' />
+                            <input className={` block my-4 p-2 w-full appearance-none text-black bg-neutral-200 `} type='email' placeholder='Correo' />
+                            <input className={` block my-4 p-2 w-full appearance-none text-black bg-neutral-200 `} type='tel' placeholder='Teléfono' />
+                            <input className={` block my-4 p-2 w-full appearance-none text-black bg-neutral-200 `} type='text' placeholder='Asunto' />
+                            <textarea className={` block my-4 p-2 w-full appearance-none text-black bg-neutral-200 `} rows='8' placeholder='Mensaje'></textarea>
+                            <input className={` inline-block mt-4 appearance-none transition-all ease-in-out duration-200 rounded-full py-1 px-12 uppercase hover:bg-white bg-[#b10c72] hover:text-black text-white no-underline font-medium cursor-pointer `} type='submit' value='Enviar' />
                         </form>
 
 
--- components/header.jsx
+++ components/header.jsx
@@ -1,31 +1,38 @@
+"use client";
 import Image from 'next/image';
 import Link from 'next/link';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+import AOS from 'aos';
 
-import impulso360Logo from 'public/images/logo.png'
+import impulso360Logo from 'public/images/logob.png'
 
 const navItems = [
-    { linkText: 'Inicio', href: '/' },
-    { linkText: 'Servicios', href: '/' },
-    { linkText: 'Quienes somos', href: '/' },
-    { linkText: 'Contacto', href: '/' }
+    { linkText: 'Inicio', href: '#inicio' },
+    { linkText: 'Servicios', href: '#serviciosIntegrales' },
+    { linkText: 'Quienes somos', href: '#porQueNosotros' },
+    { linkText: 'Contacto', href: '#contacto' }
 ];
 
 export function Header() {
+    useEffect(() => {
+      AOS.init();
+    }, []);
     return (
         <nav className=" fixed top-0 left-0 z-50 w-full font-Roboto py-6 bg-black bg-opacity-70 text-white">
 
             {!!navItems?.length && (
                 <ul className=" max-w-5xl mx-auto flex flex-wrap gap-x-6 justify-center items-center">
                     <li>
-                        <Link href="/">
-                            <Image src={impulso360Logo} className=" align-middle w-auto lg:h-11 md:h-8 h-0" alt="Netlify logo" />
+                        <Link href="#inicio">
+                            <Image src={impulso360Logo} className=" align-middle w-auto lg:h-11 md:h-8 h-0 opacity-80 " alt="Netlify logo" />
                         </Link>
                     </li>
                     {navItems.map((item, index) => (
                         <li key={index}>
                             <Link
                                 href={item.href}
-                                className="inline-block text-sm lg:text-lg transition hover:opacity-80 font-bold uppercase no-underline hover:text-[#fc27aa]"
+                                className="inline-block text-sm lg:text-lg transition opacity-80 font-bold uppercase no-underline hover:text-[#fc27aa]"
                             >
                                 {item.linkText}
                             </Link>
