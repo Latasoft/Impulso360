9fd9ee92db88c602bd6196102fb11e13ae7109e8
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-08-05 08:44:20 -0400

    sfsd

--- components/header.jsx
+++ components/header.jsx
@@ -40,7 +40,7 @@ export function Header() {
   }, []);
 
     return (
-        <nav id="navbar" className="opacity-0 border-b-2 border-solid border-white border-opacity-30 fixed top-0 left-0 z-50 w-full font-Roboto py-3 bg-black bg-opacity-70 text-white transition-transform transform -translate-y-full ease-in-out duration-[618ms]">
+        <nav id="navbar" className="opacity-0 border-b-2 border-solid border-white border-opacity-30 fixed top-0 left-0 z-50 w-full font-Roboto py-3 bg-black bg-opacity-70 text-white transition-all -translate-y-full ease-in-out duration-[618ms]">
 
             {!!navItems?.length && (
                 <ul className=" max-w-5xl mx-auto flex flex-wrap gap-x-8 justify-center items-center">
